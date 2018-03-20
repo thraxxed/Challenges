6013e1f6cb46b0d328f3c3f1c78ed161694222c7
seth <sealittl@ucsc.edu>
2018-03-20 15:43:15 -0700

    refactor

--- codefights/extractEachKth.rb
+++ codefights/extractEachKth.rb
@@ -1,7 +1,7 @@
 # Given array of integers, remove each kth element from it.
 
 def extractEachKth(inputArray, k)
-    target_idx = (k-1)
+    target_idx = k - 1
     result = []
     inputArray.each_with_index do |el, i|
        if i == target_idx
